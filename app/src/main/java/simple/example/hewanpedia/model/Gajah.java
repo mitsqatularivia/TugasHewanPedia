1fbf0a4d65692ab3c056e04bced254873490c70c
mitsqatularivia <mitsqatularivia@gmail.com>
2020-11-24 17:16:25 +0700

    Tugas

---
+++ app/src/main/java/simple/example/hewanpedia/model/Gajah.java
@@ -0,0 +1,10 @@
+package simple.example.hewanpedia.model;
+
+public class Gajah extends Hewan{
+
+    public Gajah (String ras, String asal, String deskripsi, int drawableRes) {
+        super("Gajah",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
